47fcd68b9cd174c7b91a759887ff671ee978dbf9
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-13 14:48:55 -0300

    chore: txt fuction implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+import os.path
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+    with open(path_file) as file:
+        result = [line.strip() for line in file.readlines()]
+        return result
