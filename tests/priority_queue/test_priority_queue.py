f1e807c03021d23400e76097a6f200e6dbf99081
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-13 16:36:54 -0300

    test: priority queue test implemented

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,48 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    files_size = [
+        {"qtd_linhas": 9},
+        {"qtd_linhas": 4},
+        {"qtd_linhas": 2},
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 7},
+        {"qtd_linhas": 11},
+        {"qtd_linhas": 3},
+    ]
+    for file in files_size:
+        queue.enqueue(file)
+
+    enqueue_result = queue.high_priority._data + queue.regular_priority._data
+    expected_enqueue = [
+        {"qtd_linhas": 4},
+        {"qtd_linhas": 2},
+        {"qtd_linhas": 3},
+        {"qtd_linhas": 9},
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 7},
+        {"qtd_linhas": 11},
+    ]
+    assert enqueue_result == expected_enqueue
+
+    for n in range(2):
+        queue.dequeue()
+
+    dequeue_result = queue.high_priority._data + queue.regular_priority._data
+    expected_dequeue = [
+        {"qtd_linhas": 3},
+        {"qtd_linhas": 9},
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 7},
+        {"qtd_linhas": 11},
+    ]
+    assert dequeue_result == expected_dequeue
+
+    file = queue.search(0)
+    assert file == {"qtd_linhas": 3}
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(10)
