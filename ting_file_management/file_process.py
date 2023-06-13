3fa41fd672e4b74c2d376a444fa93468dfa51738
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-13 15:28:15 -0300

    chore: txt process fuction implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    process_exist = False
+    for process in range(len(instance._data)):
+        data = instance.search(process)
+        if data == path_file:
+            process_exist = not process_exist
+            break
+
+    if not process_exist:
+        instance.enqueue(path_file)
+        file = txt_importer(path_file)
+        file_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+        return print(file_dict, file=sys.stdout)
 
 
 def remove(instance):
