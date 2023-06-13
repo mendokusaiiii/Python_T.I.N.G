246f4961ddb3a80d0d3b0bf7331fae3095d5d707
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-13 17:21:09 -0300

    chore: word search fuctions implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,41 +6,36 @@ def process(path_file, instance):
     process_exist = False
     for process in range(len(instance._data)):
         data = instance.search(process)
-        if data == path_file:
+        if data["nome_do_arquivo"] == path_file:
             process_exist = not process_exist
             break
 
     if not process_exist:
-        instance.enqueue(path_file)
         file = txt_importer(path_file)
         file_dict = {
             "nome_do_arquivo": path_file,
             "qtd_linhas": len(file),
             "linhas_do_arquivo": file,
         }
+        instance.enqueue(file_dict)
         return print(file_dict, file=sys.stdout)
 
 
 def remove(instance):
-    remove_process = instance.dequeue()
-    if remove_process is None:
+    file = instance.dequeue()
+    if file is None:
         message_error = "Não há elementos"
         print(message_error, file=sys.stdout)
     else:
-        message = f"Arquivo {remove_process} removido com sucesso"
+        path_file = file["nome_do_arquivo"]
+        message = f"Arquivo {path_file} removido com sucesso"
         print(message, file=sys.stdout)
 
 
 def file_metadata(instance, position):
     try:
         process_path_file = instance.search(position)
-        file = txt_importer(process_path_file)
-        file_dict = {
-            "nome_do_arquivo": process_path_file,
-            "qtd_linhas": len(file),
-            "linhas_do_arquivo": file,
-        }
-        return print(file_dict, file=sys.stdout)
+        return print(process_path_file, file=sys.stdout)
     except IndexError:
         message_error = "Posição inválida"
         return print(message_error, file=sys.stderr)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,36 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return report(word, instance, "simple")
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return report(word, instance, "complete")
+
+
+def report(word, instance, report_type):
+    result = []
+    for process in instance._data:
+        lines = process["linhas_do_arquivo"]
+        occurrences = process_occurrences(word, lines, report_type)
+
+        if len(occurrences) != 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": process["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return result
+
+
+def process_occurrences(word, lines, report_type):
+    occurrences = []
+    for index in range(len(lines)):
+        if word in lines[index].lower():
+            if report_type == "simple":
+                occurrences.append({"linha": index + 1})
+            else:
+                occurrences.append(
+                    {"linha": index + 1, "conteudo": lines[index]}
+                )
+    return occurrences
